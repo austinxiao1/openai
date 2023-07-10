89dbec9a60c81971f28f5ca57946f32bae91e0c8
Austin <austinxiao1@yahoo.com>
2023-07-09 20:23:33 -0700

    shop

--- hello_world_project/shop/urls.py
+++ hello_world_project/shop/urls.py
@@ -6,4 +6,6 @@ from shop import views
 urlpatterns = [
     path("",  RedirectView.as_view(url='/shop/home')), 
     path("shop/home", views.home, name="hello_there"),
+    path("shop/admin/home", views.admin_home, name="admin_home"),
+    path("shop/admin/product_add", views.admin_product_add, name="admin_product_add"),
 ]
--- hello_world_project/shop/views.py
+++ hello_world_project/shop/views.py
@@ -2,9 +2,31 @@ import json
 from django.http import Http404, HttpResponse
 from django.shortcuts import HttpResponseRedirect, get_object_or_404, redirect, render
 from django.utils.timezone import datetime
-from django.views.generic import ListView 
+from django.views.generic import ListView
+from shop.forms import ProductAddForm
+
+from shop.models.product import Product 
 
 
 def home(request):
     return render(request, "shop/home.html")
+
+
+def admin_home(request):
+    print("admin_home:")
+    context = {}
+    # play_list=OpenAIPlay.objects.order_by("-create_date")[:50],  # :5 limits the results to the five most recent
+    context["product_list"] = Product.objects.all().order_by("created_at") 
+
+    return render(request, "shop/admin/product_list.html", context)
+
+
+def admin_product_add(request):
+    form = ProductAddForm(request.POST or None)
+    print("admin_product_add:")
+    context = {}
+    # play_list=OpenAIPlay.objects.order_by("-create_date")[:50],  # :5 limits the results to the five most recent
+    context["form"] =form 
+
+    return render(request, "shop/admin/product_add.html", context)
   
